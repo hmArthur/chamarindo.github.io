f358b37e2b494c057ff22773afedc01ffa2a4ea7
hmArthur <82618569+hmArthur@users.noreply.github.com>
2025-03-15 13:11:34 -0300

    Add files via upload

--- javascript/stickyScroll.js
+++ javascript/stickyScroll.js
@@ -11,7 +11,7 @@ document.addEventListener("scroll", () => {
         menu.style.filter = "invert(1)"
     } else {
         nav.classList.remove("sticky")
-        openMenuScroll.style.top = "5rem";
+        openMenuScroll.style.top = "6rem";
          menu.style.filter = "invert(0)"
     }
 })
